83eca94014be61fe1c865c79e903b91a6e06454e
Arushi Rai <arushirai1999@gmail.com>
2019-02-24 10:52:47 -0600

    Add flights

--- app.js
+++ app.js
@@ -28,22 +28,43 @@ app.post('/inspirationSearch', (req, res) => {
     });
 });
 
+getCityCode = async (cityKeyword) => {
+    let cities = await amadeus.referenceData.locations.get({
+        keyword : cityKeyword,
+        subType : Amadeus.location.city
+    })
+    console.log(cities.data[0].address.cityCode)
+    return {cityCode: cities.data[0].address.cityCode, location: cities.data[0].geoCode}
+};
+
 app.post('/book', async (req, res) => {
     try {
+        let {cityCode: originCityCode, location: originLocation} = await getCityCode(req.body.originCityCode)
+        let {cityCode: destCityCode, location: destLocation} = await getCityCode(req.body.destCityCode)
+        console.log(originCityCode)
         let airports = await amadeus.referenceData.locations.airports.get({
-            longitude : parseFloat(req.body.longitude),
-            latitude  : parseFloat(req.body.latitude)
-        })
+                                longitude : parseFloat(destLocation.longitude),
+                                latitude  : parseFloat(destLocation.latitude)
+                            })
 
+        let flightOffers = await amadeus.shopping.flightOffers.get({
+                                    origin : originCityCode,
+                                    destination : destCityCode,
+                                    departureDate : req.body.startDate
+                                })
+        const newData = flightOffers.data.filter(flight => {
+            return flight.offerItems[0].price < req.body.flightBudget
+        })
+                        
         let hotels = await amadeus.shopping.hotelOffers.get({
-            cityCode : 'CHI' //"req.originCityCode"
+            cityCode : destCityCode     
         })
         
         hotels.result.data.filter(h => {return (h.hotel.rating > req.body.minStars) && (h.offers[0].price.total<req.body.budget)}).sort((a,b)=> {
             if(a.hotel.hotelDistance > b.hotel.hotelDistance) return 1; if (b.hotel.hotelDistance > a.hotel.hotelDistance) return -1; return 0;
         });
 
-        res.status(200).send(hotels.result.data)
+        res.status(200).send({flights: newData, hotels: hotels.result.data})
     } catch(responseError) {
         console.log(responseError);
         res.status(400).send(responseError)
